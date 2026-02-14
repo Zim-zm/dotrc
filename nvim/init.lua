2099bf83e7f06a1484bd7a5d749eb33866568672
Stephane Zimmermann <stephane.zimmermann@gmail.com>
2026-02-14 18:05:34 +0100

    Append merlin's path only if it is installed.

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,5 +4,7 @@ require("zim")
 -- Package manager. lazy seems to be the community consensus for now.
 require("config.lazy")
 
-local opamshare = io.popen('opam var share'):read()
-vim.opt.runtimepath:append(opamshare .. "/merlin/vim")
+-- local opamshare = io.popen('opam var share'):read()
+-- if opamshare  then
+--     vim.opt.runtimepath:append(opamshare .. "/merlin/vim")
+-- end
